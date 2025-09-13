b8d3cc474569f41e726a1356cf7d402c13d40b71
Collin Meyer <collinmmeyer@gmail.com>
2025-09-13 19:07:34 -0400

    don't fail in entrpoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,11 +6,6 @@ else
     MODE=$1
 fi
 
-if [ ! -f /conf.yaml ]; then
-    echo "/conf.yaml not found, aborting..."
-    exit 1
-fi
-
 if [ "$MODE" = "dev" ]; then
     uvicorn --host 0.0.0.0 --port 8000 --reload myrss.app:app --reload-dir /app/src
     exit 0
